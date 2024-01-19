6c5ddb560ec07f6f45051ecf5a366a56527e69d9
kmargosb <cmargosb@gmail.com>
2024-01-19 11:05:54 +0100

    carrito con localstorage, solo falta los botones de sumar y restar

--- app/context/DataCoffee.jsx
+++ app/context/DataCoffee.jsx
@@ -1,41 +1,51 @@
-"use client";
+'use client'
 
 import React, { createContext, useState, useEffect, useReducer } from "react";
-import cartReducer from '../../utils/cartReducer'
+import cartReducer from '../../utils/cartReducer';
 
 export const DataCoffeeContext = createContext(null);
 
-export default function DataCoffeeContextProvider({ children }) {
+const DataCoffeeContextProvider = ({ children }) => {
+  const [coffeeData, setCoffeeData] = useState(null);
+  const textTitleHome = 'Novedades';
+  const textTitleShop = 'Últimos orígenes';
+  const styleHome = `flex gap-6 w-[1200px]`;
+  const styleShop = `flex flex-wrap gap-6 w-[1200px]`;
+  const limit = 4;
 
-	
-	const [coffeeData, setCoffeeData] = useState(null);
-	const textTitleHome = 'Novedades'
-	const textTitleShop = 'Últimos orígenes'
-	const styleHome = `flex gap-6 w-[1200px]`
-	const styleShop = `flex flex-wrap gap-6  w-[1200px]`
-	const limit = 4
+  // Obtener el estado inicial del carrito desde el localStorage
+  const initialCartState = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('cart')) || [] : [];
 
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const response = await fetch('https://cafe-de-altura.vercel.app/api/products');
-				const data = await response.json();
-				const originalProducts = [...data.products];
-				originalProducts.sort((a, b) => a.available && !b.available ? -1 : !a.available && b.available ? 1 : a.price - b.price);
-				setCoffeeData(originalProducts);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		getData();
-	}, []);
+  // cart
+  const [cart, dispatch] = useReducer(cartReducer, initialCartState);
 
-	// cart
-	  const [cart, dispatch] = useReducer(cartReducer, []);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch('https://cafe-de-altura.vercel.app/api/products');
+        const data = await response.json();
+        const originalProducts = [...data.products];
+        originalProducts.sort((a, b) => (a.available && !b.available ? -1 : !a.available && b.available ? 1 : a.price - b.price));
+        setCoffeeData(originalProducts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
 
-	return (
-		<DataCoffeeContext.Provider value={{ coffeeData, textTitleHome, textTitleShop, styleShop, styleHome, limit, cart, dispatch }}>
-			{children}
-		</DataCoffeeContext.Provider>
-	)
-}
+  // Guardar el estado del carrito en el localStorage cada vez que cambie
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+		localStorage.setItem('cart', JSON.stringify(cart));
+	  }
+	}, [cart]);
+
+  return (
+    <DataCoffeeContext.Provider value={{ coffeeData, textTitleHome, textTitleShop, styleShop, styleHome, limit, cart, dispatch }}>
+      {children}
+    </DataCoffeeContext.Provider>
+  );
+};
+
+export default DataCoffeeContextProvider;
--- utils/cartReducer.jsx
+++ utils/cartReducer.jsx
@@ -1,39 +1,42 @@
 const cartReducer = (state, dispatch) => {
-    
-
     switch (dispatch.action) {
         case "ADD_ITEM":
-            const cafe = state.find((producto) => producto.id === dispatch.product._id)
+            const cafe = state.find((producto) => producto.id === dispatch.product._id);
+            let updatedState;
             if (!cafe) {
-                return [...state,
-                {
-                    id: dispatch.product._id,
-                    name: dispatch.product.brand,
-                    price: dispatch.product.price,
-                    img: dispatch.product.img_url,
-                    quantity: 1
-
-                }];
-            }
-            else {
-                return state.map((producto) =>
+                updatedState = [
+                    ...state,
+                    {
+                        id: dispatch.product._id,
+                        name: dispatch.product.brand,
+                        price: dispatch.product.price,
+                        img: dispatch.product.img_url,
+                        quantity: 1,
+                    },
+                ];
+            } else {
+                updatedState = state.map((producto) =>
                     producto.id === dispatch.product._id
                         ? { ...producto, quantity: producto.quantity + 1 }
                         : producto
                 );
             }
+            // Actualizar el localStorage
+            localStorage.setItem('cart', JSON.stringify(updatedState));
+            return updatedState;
 
         case "REMOVE_ITEM":
-            return state.filter((item) => item.id !== dispatch.product._id);
+            const newState = state.filter((item) => item.id !== dispatch.product._id);
+            // Actualizar el localStorage
+            localStorage.setItem('cart', JSON.stringify(newState));
+            return newState;
 
         default:
             return {
                 ...state,
-                total: calculateTotal(state), // Calcula el total al inicio y cada vez que se realice una acción
+                total: calculateTotal(state),
             };
     }
 };
-
-
 
 export default cartReducer;
